47435a6db36c3f228d61c9f44a0e21858af2a071
Roarco <sebuscaroberth@gmail.com>
2021-09-14 16:56:25 -0500

    implementando webpack en nuestro proyecto

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
+/* Este es el punto de entrada, aqui importamos react, asi como react-dom y todos aquellos
+componetes o archivos que requiera nuesta aplicacion.
+*/
+
 import React from "react";
 import ReactDOM  from "react-dom";
 import HelloWorld from "./components/HelloWorld";
 
-
+/* con ReactDOM.render empujamos el componente al html */
 ReactDOM.render(< HelloWorld/>, document.getElementById(app));
 
 
