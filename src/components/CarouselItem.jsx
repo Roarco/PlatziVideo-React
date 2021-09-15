f07bc9ff64728d8f47bf16dbe709229ee07fab46
Roarco <sebuscaroberth@gmail.com>
2021-09-15 18:17:43 -0500

    Añadiendo imagenes con webpack

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import '../assets/styles/components/CarouselItem.scss';
+import playIcon from '../assets/static/icons-play.png';
+import plusIcon from '../assets/static/icons-plus.png';
 
 const CarouselItem = () => (
   <div className='carousel-item'>
     <img className='carousel-item__img' src='https://images.pexels.com/photos/789822/pexels-photo-789822.jpeg?auto=format%2Ccompress&cs=tinysrgb&dpr=2&h=750&w=1260' alt='woman' />
     <div className='carousel-item__details'>
       <div>
-        <img className='carousel-item__details-img' src='/imagenes/icons-play.png' alt='play' />
-        <img className='carousel-item__details-img' src='/imagenes/icons-plus.png' alt='plus' />
+        <img className='carousel-item__details-img' src={playIcon} alt='play' />
+        <img className='carousel-item__details-img' src={plusIcon} alt='plus' />
       </div>
       <p className='carousel-item__details--title'>Titulo descriptivo</p>
       <p className='carousel-item__details--subtitle'>2019 16+ 114 minutos</p>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,14 +3,16 @@ react y en algunos casos si usamos componetes stateFul tambien usaremos el compo
 react */
 import React from 'react';
 import '../assets/styles/components/Header.scss';
+import logo from '../assets/static/logo-platzi-video-BW2.png';
+import userIcon from '../assets/static/user-icon.png';
 
 /* crearemos componentes presentacionales, con un return explicito*/
 const Header = () => (
   <header className='header'>
-    <img className='header__img' src='/imagenes/logo-platzi-video-BW2.png' alt='Platzi-Video' />
+    <img className='header__img' src={logo} alt='Platzi-Video' />
     <div className='header__menu'>
       <div className='header__menu--profile'>
-        <img src='/imagenes/user-icon.png' alt='User' />
+        <img src={userIcon} alt='User' />
         <p>Perfil</p>
       </div>
       <ul>
